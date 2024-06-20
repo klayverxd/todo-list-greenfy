fd350cd5b3c78304ba2da0a1f39f049bc9b4ddb4
Klayver Ximenes <klayverx@gmail.com>
2024-06-19 21:52:25 -0300

    chore: auth custom hook

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { UseAuthReturnType } from "../types/Auth";
+
+const useAuth = (): UseAuthReturnType => {
+	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+	useEffect(() => {
+		const token = localStorage.getItem("authToken");
+
+		if (token) {
+			setIsAuthenticated(true);
+		}
+	}, []);
+
+	const handleLogin = (token: string) => {
+		localStorage.setItem("authToken", token);
+		setIsAuthenticated(true);
+	};
+
+	const handleLogout = () => {
+		localStorage.removeItem("authToken");
+		setIsAuthenticated(false);
+	};
+
+	return {
+		isAuthenticated,
+		handleLogin,
+		handleLogout,
+	};
+};
+
+export default useAuth;
--- src/types/Auth.ts
+++ src/types/Auth.ts
@@ -1,3 +1,9 @@
 export interface AuthResponse {
   token: string;
 }
+
+export interface UseAuthReturnType {
+  isAuthenticated: boolean;
+  handleLogin: (token: string) => void;
+  handleLogout: () => void;
+}
